8641ff8e3629dfb8cdce96f06b5bc955d0c2459e
AadityaDev <adityaverma.dev@gmail.com>
2018-07-26 18:04:06 +0530

    card model added

--- app/src/main/java/com/technawabs/app/mharo/base/model/CardDetail.java
+++ app/src/main/java/com/technawabs/app/mharo/base/model/CardDetail.java
@@ -3,7 +3,21 @@ package com.technawabs.app.mharo.base.model;
 public class CardDetail {
 
     private String cardId;
-    private String user;
+    private String cardName;
 
+    public String getCardId() {
+        return cardId;
+    }
 
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
 }
